c4727b90fbe04b4acc36d0d0f23b8908347a995f
David Dorward <david@dorward.me.uk>
2026-08-23 10:19:32 +0100

    Highlight day changes

--- src/Components/event-grid/DayOfWeekCell.tsx
+++ src/Components/event-grid/DayOfWeekCell.tsx
@@ -1,5 +1,11 @@
 import { slotData } from "../../util/slotData";
 
+const HOURS_PER_DAY = 24;
+const MINUTES_PER_HOUR = 60;
+const SECONDS_PER_MINUTE = 60;
+const MS_PER_SECOND = 1000;
+const MS_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MS_PER_SECOND;
+
 interface DayOfWeekCellProps {
   day: string;
   rowSpan?: number;
@@ -7,8 +13,10 @@ interface DayOfWeekCellProps {
 
 export const DayOfWeekCell = ({ day, rowSpan }: DayOfWeekCellProps) => {
   const { dayOfWeek } = slotData(day);
+  const dayIndex = Math.floor(new Date(`${day}T00:00:00`).getTime() / MS_PER_DAY);
+  const dayParity = dayIndex % 2 === 0 ? "even" : "odd";
   return (
-    <td rowSpan={rowSpan} className="dayOfWeek">
+    <td rowSpan={rowSpan} className={`dayOfWeek ${dayParity}`}>
       {dayOfWeek}
     </td>
   );
