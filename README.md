b576743f59229f2124ef90b31cce578e852423da
David Dorward <david@dorward.me.uk>
2026-08-23 10:23:19 +0100

    Github pages

--- README.md
+++ README.md
@@ -4,8 +4,4 @@ A web app to parse a collection of proposed events from an Excel file and create
 
 ## TODO
 
-- Errors in context.
-- Update events with changes (function in context)
-- Listen somewhere to regenerate the errors when the events list changes.
-- Look and feel
 - Update PeopleList so that it highlights desirability, shows waitlist, and shows empty spaces
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,19 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+const getBase = () => {
+  const repository = process.env.GITHUB_REPOSITORY;
+  if (!repository) {
+    return "/";
+  }
+
+  const [, repositoryName] = repository.split("/");
+  return repositoryName.endsWith(".github.io") ? "/" : `/${repositoryName}/`;
+};
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: getBase(),
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
