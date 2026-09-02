338c2516b6ba95300f918e43f71e6909ca81491d
David Dorward <david@dorward.me.uk>
2026-09-02 19:51:37 +0100

    Rearrange menu
    The menu has gotten quite long. Split it over multiple rows.

--- src/Components/Menu/Navigation/Navigation.tsx
+++ src/Components/Menu/Navigation/Navigation.tsx
@@ -1,41 +1,59 @@
 import type { View } from "../../../types";
 import { NavButton } from "./NavButton";
 
-const pages = [
-  {
-    label: "Attendees",
-    name: "attendees",
-  },
-  {
-    label: "Personalised Schedules",
-    name: "schedules",
-  },
-  {
-    label: "Event List",
-    name: "event-list",
-  },
-  {
-    label: "Event Grid",
-    name: "event-grid",
-  },
-  {
-    label: "Signup Sheets",
-    name: "signup-sheets",
-  },
-  {
-    label: "Room Map",
-    name: "room-map",
-  },
-] as const;
+interface NavigationItem {
+  label: string;
+  name: View;
+}
 
 interface NavigationProps {
   changePage: React.ButtonHTMLAttributes<HTMLButtonElement>["onClick"];
   currentView: View;
 }
 
-export const Navigation = ({ changePage, currentView }: NavigationProps) =>
-  pages.map(({ name, label }) => (
+const people: readonly NavigationItem[] = [
+  {
+    label: "Attendees",
+    name: "attendees",
+  },
+  {
+    label: "Room Map",
+    name: "room-map",
+  },
+  {
+    label: "Personalised Schedules",
+    name: "schedules",
+  },
+];
+
+const games: readonly NavigationItem[] = [
+  {
+    label: "Games",
+    name: "event-list",
+  },
+  {
+    label: "Game Calendar",
+    name: "event-grid",
+  },
+  {
+    label: "Signup Sheets",
+    name: "signup-sheets",
+  },
+];
+
+const generateButtons = (
+  items: readonly NavigationItem[],
+  { changePage, currentView }: NavigationProps,
+): React.ReactNode =>
+  items.map(({ name, label }: NavigationItem) => (
     <NavButton key={name} value={name} changePage={changePage} currentView={currentView}>
       {label}
     </NavButton>
   ));
+
+export const Navigation = (props: NavigationProps) => (
+  <div>
+    <div className="button-group">{generateButtons(people, props)}</div>
+    <div className="button-group">{generateButtons(games, props)}</div>
+  </div>
+);
